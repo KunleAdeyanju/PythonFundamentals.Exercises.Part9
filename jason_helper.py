4deaa557dc925af1e8c9c3f9b562b37f634d0e48
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-03-19 10:01:37 -0400

    write and load pickle written

--- jason_helper.py
+++ jason_helper.py
@@ -10,8 +10,17 @@ def read_all_json_files(file_path):
     jlist = []
     for file in os.lisdir(file_path):
         full_file_path = "%s/%s" % (file_path, file)
-        with open(full_file_path, 'r') as file2
-        data = json.load(file2)
+        data = read_json(full_file_path)
         jlist.append(data)
     
     return jlist
+
+
+def write_pickle(file_path, data):
+    json_object = json.dumps(data, indent=4)
+    with open("super_smash_characters.pickle", mode = "w") as outfile:
+        outfile.write(json_object)
+
+def load_pickle(file_path):
+    pickle = read_json(file_path)
+    print(pickle)
