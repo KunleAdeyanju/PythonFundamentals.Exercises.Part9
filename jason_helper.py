977b5d879f9bc58d2864dbb838bd37ef9e7ec6f2
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-03-19 09:23:03 -0400

    fixed

--- jason_helper.py
+++ jason_helper.py
@@ -2,7 +2,8 @@ import json
 import os
 
 def read_json(file_path):
-    
+    with open(file_path, 'r') as file:
+        data = json.load(file)
     return data
 
 def read_all_json_files(file_path):
